f1fa508a3a436d808d3480c0bbd4c3da5ee1b344
Shradha Sehgal <shradha.sgl@gmail.com>
2019-04-14 01:09:25 +0530

    Rendered all pages using flask

---
+++ views.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def introduction():
+	return render_template('Introduction.html', topic ='Introduction')
+
+@app.route("/experiment")
+def experiment():
+	return render_template('Experiment.html', topic ='Experiment')
+
+@app.route("/manual")
+def manual():
+	return render_template('Manual.html', topic ='Manual')
+
+@app.route("/theory")
+def theory():
+	return render_template('Theory.html', topic ='Theory')
+
+@app.route("/objective")
+def objective():
+	return render_template('Objective.html', topic ='Objective')
+
+@app.route("/quiz")
+def quiz():
+	return render_template('Quizzes.html', topic ='Quiz')
+
+@app.route("/feedback")
+def feedback():
+	return render_template('Feedback.html', topic ='Feedback')
+
+@app.route("/procedure")
+def procedure():
+	return render_template('Procedure.html', topic ='Procedure')
+
+
+if __name__ == '__main__':
+	app.run(debug = True)
